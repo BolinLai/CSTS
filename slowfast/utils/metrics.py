f20018e51bc3680fa9498283bc591c6a44b50add
TDarius <tdarius@perce1.users.iri.prv>
2024-11-18 16:01:35 +0100

    implemented metric func with reduced memory usage

--- slowfast/utils/metrics.py
+++ slowfast/utils/metrics.py
@@ -41,34 +41,81 @@ def adaptive_f1(preds, labels_hm, labels, dataset):
     else:  # gaze estimation
         thresholds = np.linspace(0, 0.02, 11)
         # thresholds = np.linspace(0, 1.0, 21)
+     
+    # all_preds = torch.zeros(size=(thresholds.shape + labels_hm.size()), device=labels_hm.device)
+    # all_labels = torch.zeros(size=(thresholds.shape + labels_hm.size()), device=labels_hm.device)
+    # binary_labels = (labels_hm > 0.001).int()  # change to 0.001
+    # for i in range(thresholds.shape[0]):  # There is some space for improvement. You can calculate f1 in the loop rather than save all preds. It consumes much memory.
+    #     binary_preds = (preds.squeeze(1) > thresholds[i]).int()
+    #     all_preds[i, ...] = binary_preds
+    #     all_labels[i, ...] = binary_labels
+    # tp = (all_preds * all_labels).sum(dim=(3, 4))
+    # fg_labels = all_labels.sum(dim=(3, 4))
+    # fg_preds = all_preds.sum(dim=(3, 4))
+    # 
+    # if dataset == 'egteagaze':
+    #     fixation_idx = 1
+    # elif dataset in ['ego4dgaze', 'ego4dgaze_forecast', 'ego4d_av_gaze', 'ego4d_av_gaze_forecast', 'aria_gaze',
+    #                  'aria_gaze_forecast', 'aria_av_gaze', 'aria_av_gaze_forecast']:
+    #     fixation_idx = 0
+    # else:
+    #     raise NotImplementedError(f'Metrics of {dataset} is not implemented.')
+    # labels_flat = labels.view(labels.size(0) * labels.size(1), labels.size(2))
+    # tracked_idx = torch.where(labels_flat[:, 2] == fixation_idx)[0]
+    # tp = tp.view(tp.size(0), tp.size(1)*tp.size(2)).index_select(1, tracked_idx)
+    # fg_labels = fg_labels.view(fg_labels.size(0), fg_labels.size(1)*fg_labels.size(2)).index_select(1, tracked_idx)
+    # fg_preds = fg_preds.view(fg_preds.size(0), fg_preds.size(1)*fg_preds.size(2)).index_select(1, tracked_idx)
+    # recall = (tp / (fg_labels + 1e-6)).mean(dim=1)
+    # precision = (tp / (fg_preds + 1e-6)).mean(dim=1)
+    # f1 = (2 * recall * precision) / (recall + precision + 1e-6)
+    # max_idx = torch.argmax(f1)
+    # 
+    # return float(f1[max_idx].cpu().numpy()), float(recall[max_idx].cpu().numpy()), \
+    #        float(precision[max_idx].cpu().numpy()), thresholds[max_idx]  # need np.float64 in logging rather than np.float32
 
-    all_preds = torch.zeros(size=(thresholds.shape + labels_hm.size()), device=labels_hm.device)
-    all_labels = torch.zeros(size=(thresholds.shape + labels_hm.size()), device=labels_hm.device)
     binary_labels = (labels_hm > 0.001).int()  # change to 0.001
-    for i in range(thresholds.shape[0]):  # There is some space for improvement. You can calculate f1 in the loop rather than save all preds. It consumes much memory.
-        binary_preds = (preds.squeeze(1) > thresholds[i]).int()
-        all_preds[i, ...] = binary_preds
-        all_labels[i, ...] = binary_labels
-    tp = (all_preds * all_labels).sum(dim=(3, 4))
-    fg_labels = all_labels.sum(dim=(3, 4))
-    fg_preds = all_preds.sum(dim=(3, 4))
+
+    best_f1 = 0
+    best_recall = 0
+    best_precision = 0
+    best_threshold = 0
 
     if dataset == 'egteagaze':
-        fixation_idx = 1
+            fixation_idx = 1
     elif dataset in ['ego4dgaze', 'ego4dgaze_forecast', 'ego4d_av_gaze', 'ego4d_av_gaze_forecast', 'aria_gaze',
-                     'aria_gaze_forecast', 'aria_av_gaze', 'aria_av_gaze_forecast']:
+                    'aria_gaze_forecast', 'aria_av_gaze', 'aria_av_gaze_forecast']:
         fixation_idx = 0
     else:
         raise NotImplementedError(f'Metrics of {dataset} is not implemented.')
-    labels_flat = labels.view(labels.size(0) * labels.size(1), labels.size(2))
-    tracked_idx = torch.where(labels_flat[:, 2] == fixation_idx)[0]
-    tp = tp.view(tp.size(0), tp.size(1)*tp.size(2)).index_select(1, tracked_idx)
-    fg_labels = fg_labels.view(fg_labels.size(0), fg_labels.size(1)*fg_labels.size(2)).index_select(1, tracked_idx)
-    fg_preds = fg_preds.view(fg_preds.size(0), fg_preds.size(1)*fg_preds.size(2)).index_select(1, tracked_idx)
-    recall = (tp / (fg_labels + 1e-6)).mean(dim=1)
-    precision = (tp / (fg_preds + 1e-6)).mean(dim=1)
-    f1 = (2 * recall * precision) / (recall + precision + 1e-6)
-    max_idx = torch.argmax(f1)
 
-    return float(f1[max_idx].cpu().numpy()), float(recall[max_idx].cpu().numpy()), \
-           float(precision[max_idx].cpu().numpy()), thresholds[max_idx]  # need np.float64 in logging rather than np.float32
+    for i in range(thresholds.shape[0]):
+
+        binary_preds = (preds.squeeze(1) > thresholds[i]).int()
+
+        tp = (binary_preds.unsqueeze(0) * binary_labels.unsqueeze(0)).sum(dim=(3, 4))
+        fg_labels = binary_labels.unsqueeze(0).sum(dim=(3, 4))
+
+        fg_preds = binary_preds.unsqueeze(0).sum(dim=(3, 4))
+        
+        labels_flat = labels.view(labels.size(0) * labels.size(1), labels.size(2))
+
+        tracked_idx = torch.where(labels_flat[:, 2] == fixation_idx)[0]
+
+        tp = tp.view(tp.size(0), tp.size(1)*tp.size(2)).index_select(1, tracked_idx)
+        fg_labels = fg_labels.view(fg_labels.size(0), fg_labels.size(1)*fg_labels.size(2)).index_select(1, tracked_idx)
+        fg_preds = fg_preds.view(fg_preds.size(0), fg_preds.size(1)*fg_preds.size(2)).index_select(1, tracked_idx)
+
+
+        recall = (tp / (fg_labels + 1e-6)).mean(dim=1)
+        precision = (tp / (fg_preds + 1e-6)).mean(dim=1)
+        f1 = (2 * recall * precision) / (recall + precision + 1e-6)
+
+        max_f1 = f1.max()
+        if max_f1 > best_f1:
+            best_f1 = max_f1
+            best_recall = recall[f1.argmax()]
+            best_precision = precision[f1.argmax()]
+            best_threshold = thresholds[i]
+
+    return float(best_f1.cpu().numpy()), float(best_recall.cpu().numpy()), \
+        float(best_precision.cpu().numpy()), best_threshold
